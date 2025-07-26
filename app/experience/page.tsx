15928a4fdb4d460e957dc4b77415034fdbb07762
Hariharan S <157479313+Hariharan-Sathiyamoorthy@users.noreply.github.com>
2025-07-26 21:01:01 +0100

    changes

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -43,7 +43,7 @@ export default function Profile() {
         <div className="mb-10 ml-4">
           <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
           <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Apr 2022 - Nov 2023</time>
-          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Software Engineer - Fullstack</h3>
+          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Software Engineer</h3>
           <h4 className="text-sm text-gray-500 dark:text-gray-400">Cholamandalam Investment and Finance • Chennai, India</h4>
           <ul className="mt-4 space-y-2 pl-4">
             <li className="text-base font-normal text-gray-500 dark:text-gray-400">
@@ -78,7 +78,7 @@ implemented a CI/CD pipeline with automated testing, including unit, integration
         <div className="mb-10 ml-4">
           <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
           <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Jan 2022 - Apr 2022</time>
-          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Backend Developer</h3>
+          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Software Engineer</h3>
           <h4 className="text-sm text-gray-500 dark:text-gray-400">Intromagic • Bangalore, India</h4>
           <ul className="mt-4 space-y-2 pl-4">
             <li className="text-base font-normal text-gray-500 dark:text-gray-400">
@@ -102,7 +102,7 @@ business applications
           <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
           <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Mar 2021 - Dec 2021</time>
           <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Software Developer</h3>
-          <h4 className="text-sm text-gray-500 dark:text-gray-400">Freelance • Chennai, India</h4>
+          <h4 className="text-sm text-gray-500 dark:text-gray-400">Zero to Miles • Chennai, India</h4>
           <p className="mt-4 text-base font-normal text-gray-500 dark:text-gray-400 pl-4">
           Created several full-stack web applications, leveraging modern technologies to
 deliver end-to-end solutions. Demonstrated strong organizational skills, developed effective problem-solving
